2b4d0909c5be6c4c680b394347cda79fb524d3bf
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2020-09-17 15:12:29 +0200

    Réorganisation de la page main.js

--- main.js
+++ main.js
@@ -1,39 +1,36 @@
 ajaxGet("http://localhost:3000/api/cameras", function (reponse) {
   let cameras = JSON.parse(reponse);
+
+  // Boucle pour récupérer la liste des caméras
   cameras.forEach(function (cameraList) {
+    let productDisplay = document.getElementById("productDisplay");
+
+    // Création des éléments
     let card = document.createElement("div");
-    card.className = "card";
     let cameraImage = document.createElement("img");
-    cameraImage.src = cameraList.imageUrl;
     let cardBody = document.createElement("div");
-    cardBody.className = "card-body";
     let cameraName = document.createElement("h5");
-    cameraName.textContent = cameraList.name;
     let cameraPrice = document.createElement("p");
-    cameraPrice.textContent = cameraList.price / 100 + " €";
     let cameraDescription = document.createElement("a");
+
+    // Personnalisation des éléments
+    card.className = "card";
+    cameraImage.src = cameraList.imageUrl;
+    cardBody.className = "card-body";
+    cameraName.textContent = cameraList.name;
+    cameraPrice.textContent = cameraList.price / 100 + " €";
     cameraDescription.className = "button";
     cameraDescription.textContent = "Voir l'article";
     cameraDescription.href = "product.html?id=" + cameraList._id;
-    let productDisplay = document.getElementById("productDisplay");
-    //console.log(productDisplay);
+
+    // Ajout des éléments
     productDisplay.appendChild(card);
-    //console.log(card);
-    cameraImage.src = cameraList.imageUrl;
     card.appendChild(cameraImage);
-    //console.log(cameraImage);
-
     card.appendChild(cardBody);
-    //console.log(cardBody);
-    cameraName.textContent = cameraList.name;
     cardBody.appendChild(cameraName);
-    //console.log(cameraName);
-    cameraPrice.textContent = cameraList.price / 100 + " €";
     cardBody.appendChild(cameraPrice);
-
     cardBody.appendChild(cameraDescription);
   });
 });
 
 console.log(productDisplay);
-
