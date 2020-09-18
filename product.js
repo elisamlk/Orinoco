f548f3425ba2624a5bd814f72caf5fef16a2d458
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2020-09-18 11:53:08 +0200

    AJout de la boucle pour le choix des lentilles

--- product.js
+++ product.js
@@ -6,53 +6,64 @@ console.log(url);
 
 ajaxGet(url, function (response) {
   let cameraDescription = JSON.parse(response);
-  console.log(cameraDescription);
 
   // Récupération de l'élement main
   let productDescription = document.getElementById("productDescription");
 
-  // Création des éléments
+  // Création div
   let cardDescription = document.createElement("div");
-  let imageDescription = document.createElement("img");
-  let bodyDescription = document.createElement("div");
-  let titleDescription = document.createElement("h5");
-  let itemDescription = document.createElement("p");
-  let itemPrice = document.createElement("p");
-  let select = document.createElement("select");
-  let option = document.createElement("option");
-  let addToCart = document.createElement("button");
-  console.log(typeof select);
-
-  // Personnalisation des éléments
   cardDescription.className = "cardDescription";
+  productDescription.appendChild(cardDescription);
+
+  // Création de l'image
+  let imageDescription = document.createElement("img");
   imageDescription.className = "imageDescription";
+  imageDescription.src = cameraDescription.imageUrl;
+  cardDescription.appendChild(imageDescription);
+
+  // Création de la div bodyDescription
+  let bodyDescription = document.createElement("div");
   bodyDescription.className = "bodyDescription";
+  cardDescription.appendChild(bodyDescription);
+
+  // Création du nom de l'appareil
+  let titleDescription = document.createElement("h5");
   titleDescription.className = "titleDescription";
   titleDescription.textContent = cameraDescription.name;
+  bodyDescription.appendChild(titleDescription);
+
+  // Création de la description
+  let itemDescription = document.createElement("p");
   itemDescription.className = "itemDescription";
   itemDescription.textContent = cameraDescription.description;
+  bodyDescription.appendChild(itemDescription);
+
+  // Ajout du prix
+  let itemPrice = document.createElement("p");
   itemPrice.textContent = cameraDescription.price / 100 + " €";
+  bodyDescription.appendChild(itemPrice);
+
+  // Choix des lentilles
+  let select = document.createElement("select");
+  bodyDescription.appendChild(select);
+  let firstoption = document.createElement("option");
+  firstoption.textContent = "Sélectionner une lentille";
+  select.appendChild(firstoption);
+  for (let i = 0; i < cameraDescription.lenses.length; i++) {
+    let lenseoption = document.createElement("option");
+    lenseoption.textContent = cameraDescription.lenses[i];
+    lenseoption.setAttribute("value", cameraDescription.lenses);
+    select.appendChild(lenseoption);
+  }
+
+  // Création du boutton ajout au panier
+  let addToCart = document.createElement("button");
   addToCart.className = "button";
   addToCart.id = "addToCart";
   addToCart.textContent = "Ajouter au panier";
-  option.textContent = cameraDescription.lenses;
-
-  // boucle pour le choix des lentilles
-
-  // Ajout des éléments
-  productDescription.appendChild(cardDescription);
-  imageDescription.src = cameraDescription.imageUrl;
-  cardDescription.appendChild(imageDescription);
-  cardDescription.appendChild(bodyDescription);
-  bodyDescription.appendChild(titleDescription);
-  bodyDescription.appendChild(itemDescription);
-  bodyDescription.appendChild(itemPrice);
-  bodyDescription.appendChild(select);
-  select.appendChild(option);
-  console.log(productDescription);
   bodyDescription.appendChild(addToCart);
+  console.log(productDescription);
 
-  
   // Ajout des produits dans le panier
   let buttonAction = document.getElementById("addToCart");
   buttonAction.addEventListener("click", function (cartNumbers) {
@@ -60,10 +71,10 @@ ajaxGet(url, function (response) {
     productNumbers = parseInt(productNumbers);
     if (productNumbers) {
       localStorage.setItem("cartNumbers", productNumbers + 1);
+      document.querySelector("span").textContent = productNumbers + 1;
     } else {
       localStorage.setItem("cartNumbers", 1);
+      document.querySelector("span").textContent = 1;
     }
-
-    console.log(productNumbers);
   });
 });
