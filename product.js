b9aff05eca3ab4be004f60b6e9109f6b2bd29dde
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2020-09-22 21:41:32 +0200

    Modification de la page produit

--- product.js
+++ product.js
@@ -8,45 +8,56 @@ console.log(url);
 ajaxGet(url, function (response) {
   let cameraDescription = JSON.parse(response);
 
-  // Récupération de l'élement main
   let productDescription = document.getElementById("productDescription");
 
-  // Création div
-  let cardDescription = document.createElement("div");
-  cardDescription.className = "cardDescription";
-  productDescription.appendChild(cardDescription);
+  // Création div principale
+  let mainContainer = document.createElement("div");
+  mainContainer.className = "row no-gutters";
+  productDescription.appendChild(mainContainer);
+
+  // Création div secondaire
+  let secondContainer = document.createElement("div");
+  secondContainer.className = "col-md-4";
+  mainContainer.appendChild(secondContainer);
 
   // Création de l'image
   let imageDescription = document.createElement("img");
-  imageDescription.className = "imageDescription";
+  imageDescription.className = "card-img";
   imageDescription.src = cameraDescription.imageUrl;
-  cardDescription.appendChild(imageDescription);
+  secondContainer.appendChild(imageDescription);
 
-  // Création de la div bodyDescription
-  let bodyDescription = document.createElement("div");
-  bodyDescription.className = "bodyDescription";
-  cardDescription.appendChild(bodyDescription);
+  // Création troisième div
+  let thirdContainer = document.createElement("div");
+  thirdContainer.className = "col-md-8";
+  mainContainer.appendChild(thirdContainer);
 
-  // Création du nom de l'appareil
+  // Création div cardBody
+  let cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  thirdContainer.appendChild(cardBody);
+
+  // Création titre
   let titleDescription = document.createElement("h5");
-  titleDescription.className = "titleDescription";
+  titleDescription.className = "card-title";
   titleDescription.textContent = cameraDescription.name;
-  bodyDescription.appendChild(titleDescription);
+  cardBody.appendChild(titleDescription);
 
   // Création de la description
   let itemDescription = document.createElement("p");
-  itemDescription.className = "itemDescription";
+  itemDescription.className = "card-text";
   itemDescription.textContent = cameraDescription.description;
-  bodyDescription.appendChild(itemDescription);
+  cardBody.appendChild(itemDescription);
 
-  // Ajout du prix
+  // Création du prix
   let itemPrice = document.createElement("p");
+  itemPrice.className = "card-text";
   itemPrice.textContent = cameraDescription.price / 100 + " €";
-  bodyDescription.appendChild(itemPrice);
+  cardBody.appendChild(itemPrice);
 
   // Choix des lentilles
   let select = document.createElement("select");
-  bodyDescription.appendChild(select);
+  select.className = "card-text";
+  cardBody.appendChild(select);
   let firstoption = document.createElement("option");
   firstoption.textContent = "Sélectionner une lentille";
   select.appendChild(firstoption);
@@ -59,21 +70,20 @@ ajaxGet(url, function (response) {
 
   // Création du bouton ajout au panier
   let addToCart = document.createElement("button");
-  addToCart.className = "button";
+  addToCart.className = "card-text btn btn-md";
   addToCart.id = "addToCart";
   addToCart.textContent = "Ajouter au panier";
-  bodyDescription.appendChild(addToCart);
+  cardBody.appendChild(addToCart);
 
   let buttonAction = document.getElementById("addToCart");
 
   // Compteur panier et ajout des produits dans le localStorage
   buttonAction.addEventListener("click", function (cartNumbers) {
-
     let productNumbers = localStorage.getItem("cartNumbers");
     productNumbers = parseInt(productNumbers);
     let lenseSelect = document.querySelector("select");
     let lenseChoice = lenseSelect.selectedIndex;
-    
+
     if (lenseChoice == 0) {
       localStorage.setItem("cartNumbers", 0); // Rien ne s'ajoute au compteur panier
       document.querySelector("span").textContent = 0;
@@ -93,7 +103,8 @@ ajaxGet(url, function (response) {
       localStorage.setItem(cameraDescription._id, addToCart);
       alert("Votre article a été ajouté au panier");
       console.log("Cet article a été ajouté" + "" + addToCart);
-      
     }
   });
 });
+
+console.log(productDescription);
